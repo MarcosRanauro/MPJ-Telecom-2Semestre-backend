c69cdfa35f188d06224ab8b0337d002d49d60856
MarcosRanauro <marcosranauro@hotmail.com>
2023-10-25 01:19:15 -0300

    Css e Foto de perfil
    Fiz a implementação para o perfilMaster também conseguir colocar e deletar foto. 11

--- components/delete_image.php
+++ components/delete_image.php
@@ -11,22 +11,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])) {
     if ($row) {
         $image_path = $row['profile_image_path'];
         if (file_exists($image_path)) {
-            if (unlink($image_path)) {
-                $stmt = $pdo->prepare("UPDATE usuarios SET profile_image_path = NULL WHERE id = :id");
-                $stmt->bindParam(":id", $id, PDO::PARAM_INT);
-                $stmt->execute();
-                if ($tipo_usuario == 'master') {
-                    echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
-                } else {
-                    echo '<script>window.location.href = "../pages/perfil.php";</script>';
-                }
+            unlink($image_path);
+            $stmt = $pdo->prepare("UPDATE usuarios SET profile_image_path = NULL WHERE id = :id");
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            if($tipo_usuario == 'master') {
+              echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
             } else {
-                if ($tipo_usuario == 'master') {
-                    echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
-                } else {
-                    echo '<script>window.location.href = "../pages/perfil.php";</script>';
-                }
+              echo '<script>window.location.href = "../pages/perfil.php";</script>';
             }
+            exit();
         }
     }
 }
