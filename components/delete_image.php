925ab02959737a219b51ad7f4597526755dcd60a
MarcosRanauro <marcosranauro@hotmail.com>
2023-10-25 01:11:47 -0300

    Css e Foto de perfil
    Fiz a implementação para o perfilMaster também conseguir colocar e deletar foto. 9

--- components/delete_image.php
+++ components/delete_image.php
@@ -1,20 +1,15 @@
 <?php
 session_start();
 include_once('./config.php');
-
 $tipo_usuario = isset($_POST['tipo_usuario']) ? $_POST['tipo_usuario'] : '';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])) {
     $id = $_POST['id'];
-
     $stmt = $pdo->prepare("SELECT profile_image_path FROM usuarios WHERE id = :id");
     $stmt->bindParam(":id", $id, PDO::PARAM_INT);
     $stmt->execute();
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
     if ($row) {
         $image_path = $row['profile_image_path'];
-
         if (file_exists($image_path)) {
             if (unlink($image_path)) {
                 $stmt = $pdo->prepare("UPDATE usuarios SET profile_image_path = NULL WHERE id = :id");
@@ -22,29 +17,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])) {
                 $stmt->execute();
                 if ($tipo_usuario == 'master') {
                     echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
-                    exit();
                 } else {
                     echo '<script>window.location.href = "../pages/perfil.php";</script>';
-                    exit();
                 }
             } else {
                 echo '<script>alert("Erro ao deletar imagem");</script>';
                 if ($tipo_usuario == 'master') {
                     echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
-                    exit();
                 } else {
                     echo '<script>window.location.href = "../pages/perfil.php";</script>';
-                    exit();
                 }
-            }
-        } else {
-            echo '<script>alert("Arquivo de imagem não encontrado");</script>';
-            if ($tipo_usuario == 'master') {
-                echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
-                exit();
-            } else {
-                echo '<script>window.location.href = "../pages/perfil.php";</script>';
-                exit();
             }
         }
     }
