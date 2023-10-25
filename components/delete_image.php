8293b73879404f0617fb20a5e6282cb86919c1ad
MarcosRanauro <marcosranauro@hotmail.com>
2023-10-25 16:08:36 -0300

    Css e Foto de perfil
    Fiz a implementação para o perfilMaster também conseguir colocar e deletar foto. 16

--- components/delete_image.php
+++ components/delete_image.php
@@ -14,15 +14,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])) {
     if ($row) {
         $image_path = $row['profile_image_path'];
         if (file_exists($image_path)) {
-            print_r($image_path);
             unlink($image_path);
             $stmt = $pdo->prepare("UPDATE usuarios SET profile_image_path = NULL WHERE id = :id");
             $stmt->bindParam(":id", $id, PDO::PARAM_INT);
             $stmt->execute();
             if($tipo_usuario == 'master') {
-              //echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
+              echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
             } else {
-              //echo '<script>window.location.href = "../pages/perfil.php";</script>';
+              echo '<script>window.location.href = "../pages/perfil.php";</script>';
             }
             exit();
         }
--- components/upload.php
+++ components/upload.php
@@ -14,6 +14,7 @@ if ($_FILES["profile_image"]["error"] == 0) {
   $image_path = $upload_dir . $image_name;
 
   if (move_uploaded_file($temp_name, $image_path)) {
+    chmod($image_path, 0777);
     $userID = $id;
 
     $stmt = $pdo->prepare("UPDATE usuarios SET profile_image_path = :profile_image_path WHERE id = :id");
@@ -22,24 +23,24 @@ if ($_FILES["profile_image"]["error"] == 0) {
     $stmt->execute();
 
     if ($tipo_usuario == 'master') {
-      //header('Location: ../pages/perfilMaster.php');
+      header('Location: ../pages/perfilMaster.php');
     } else {
       echo "A imagem foi carregada com sucesso e o caminho foi atualizado no banco de dados.";
-      //header('Location: ../pages/perfil.php');
+      header('Location: ../pages/perfil.php');
     }
   } else {
     if ($tipo_usuario == 'master') {
-      //echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
+      echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
     } else {
       echo "Falha ao carregar a imagem.";
-      //echo '<script>window.location.href = "../pages/perfil.php";</script>';
+      echo '<script>window.location.href = "../pages/perfil.php";</script>';
     }
   }
 } else {
   if ($tipo_usuario == 'master') {
-    //echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
+    echo '<script>window.location.href = "../pages/perfilMaster.php";</script>';
   } else {
     echo "Erro ao carregar a imagem: " . $_FILES["profile_image"]["error"];
-    //echo '<script>window.location.href = "../pages/perfil.php";</script>';
+    echo '<script>window.location.href = "../pages/perfil.php";</script>';
   }
 }
