17faf16fd2f6b81da3a1f12da79c94b61ab4885e
MarcosRanauro <marcosranauro@hotmail.com>
2023-11-21 22:18:08 -0300

    Ajustando css
    Ajustando o css do arquivo tell.css e sms.css

--- pages/sms.php
+++ pages/sms.php
@@ -26,7 +26,6 @@ if (!isset($_SESSION['tipo_usuario'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/header_footer.css">
-    <link rel="stylesheet" href="../styles/produtos.css">
     <link rel="stylesheet" href="../styles/sms.css">
     <link rel="stylesheet" href="../fontawesome-free-6.4.0-web/css/all.min.css">
     <title>CPaaS MJP</title>
